d518737c21a902b3450c615eee00bddc3b939944
ani1999deep <anideepbhowmick1@gmail.com>
2023-04-06 13:04:34 +0530

    Add webpack

---
+++ src/App.js
@@ -0,0 +1,3 @@
+import {Other} from './Other'
+import './style.css'
+console.log(Other(),'app file')
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+//import path for change the output file
+const path = require('path')
+module.exports = {
+  mode: "production",
+  entry: './src/App.js',
+  output: {
+
+    path: path.resolve(__dirname, 'dist'),
+    filename: "output.js"
+  },
+  //loader
+  module: {
+
+    rules: [
+
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  //loader
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3500,
+  },
+};
